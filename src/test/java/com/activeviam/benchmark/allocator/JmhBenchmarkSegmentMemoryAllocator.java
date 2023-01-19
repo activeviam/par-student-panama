f9eaf19ebb33fd9547f11635947c3864e19faecc
Paul Briand <paul.briand@student-cs.fr>
2023-01-19 09:00:43 +0100

    Refactor JmhBenchmark for allocators

--- src/test/java/com/activeviam/benchmark/allocator/JmhBenchmarkSegmentMemoryAllocator.java
+++ src/test/java/com/activeviam/benchmark/allocator/JmhBenchmarkSegmentMemoryAllocator.java
@@ -1,5 +1,6 @@
 package com.activeviam.benchmark.allocator;
 
+import com.activeviam.Types;
 import com.activeviam.chunk.SegmentMemoryAllocator;
 import org.openjdk.jmh.annotations.*;
 
@@ -21,4 +22,43 @@ public class JmhBenchmarkSegmentMemoryAllocator extends JmhBenchmarkMemoryAlloca
         SESSION.close();
         setupAllocator();
     }
+
+    @Benchmark
+    public void allocateIntegerChunk() {
+        ALLOCATOR.allocateIntegerChunk(CHUNK_SIZE);
+    }
+    @Benchmark
+    public void allocateDoubleChunk() {
+        ALLOCATOR.allocateDoubleChunk(CHUNK_SIZE);
+    }
+
+    @Benchmark
+    public void allocateDoubleVectorChunk() {
+        ALLOCATOR.allocateVectorChunk(CHUNK_SIZE, Types.INTEGER);
+    }
+
+    @Param({"1000", "10000", "100000" })
+    protected static int NB_ALLOCATIONS;
+
+    @Benchmark
+    public void allocateMultipleIntegerChunk() {
+        for (int i = 0; i < NB_ALLOCATIONS; i++) {
+            ALLOCATOR.allocateIntegerChunk(1);
+        }
+    }
+
+    @Benchmark
+    public void allocateMultipleDoubleChunk() {
+        for (int i = 0; i < NB_ALLOCATIONS; i++) {
+            ALLOCATOR.allocateDoubleChunk(1);
+        }
+    }
+
+    @Benchmark
+    public void allocateMultipleVectorChunk() {
+        for (int i = 0; i < NB_ALLOCATIONS; i++) {
+            ALLOCATOR.allocateVectorChunk(1, Types.INTEGER);
+        }
+    }
+
 }

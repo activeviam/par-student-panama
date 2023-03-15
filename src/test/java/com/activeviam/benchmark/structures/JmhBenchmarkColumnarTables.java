f993b1f0dd3ff3fb5becb405f59dd7b5756c7439
Paul Briand <paul.briand@student-cs.fr>
2023-03-15 11:26:08 +0100

    Benchmark ColumnarTable.findRows(SIMD)

---
+++ src/test/java/com/activeviam/benchmark/structures/JmhBenchmarkColumnarTables.java
@@ -0,0 +1,83 @@
+/*
+ * (C) ActiveViam 2021
+ * ALL RIGHTS RESERVED. This material is the CONFIDENTIAL and PROPRIETARY
+ * property of ActiveViam. Any unauthorized use,
+ * reproduction or transfer of this material is strictly prohibited
+ */
+
+package com.activeviam.benchmark.structures;
+
+import com.activeviam.chunk.IChunkAllocator;
+import com.activeviam.chunk.SegmentMemoryAllocator;
+import com.activeviam.structures.store.IRecord;
+import com.activeviam.structures.store.impl.ColumnarTable;
+import com.activeviam.structures.store.impl.Record;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.lang.foreign.MemorySession;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
+@BenchmarkMode(Mode.AverageTime)
+@State(Scope.Thread)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = 20, time = 100, timeUnit = MILLISECONDS)
+@Measurement(iterations = 20, time = 100, timeUnit = MILLISECONDS)
+@Fork(1)
+public class JmhBenchmarkColumnarTables {
+	@Param({"1000", /* "100000", "10000000", "100000000" */})
+	protected static int TABLE_SIZE;
+	protected MemorySession memorySession;
+
+	protected IChunkAllocator CHUNK_ALLOCATOR;
+
+	@Param({"16", /* "32", "64", "128"*/})
+	protected int CHUNK_SIZE;
+
+	@Param({"10", /* "100000", "10000000", "100000000" */})
+	protected int NB_OF_ATTRIBUTES;
+
+	@Param({"10", /* "100000", "10000000", "100000000" */})
+	protected int NB_OF_VALUES;
+
+	protected static ColumnarTable ZERO_TABLE;
+
+	@Setup(Level.Iteration)
+	public void setupVectorAllocator() {
+		memorySession = MemorySession.openConfined();
+		CHUNK_ALLOCATOR = new SegmentMemoryAllocator(memorySession);
+	}
+
+	@Setup(Level.Trial)
+	public void setZeroTable() {
+		ZERO_TABLE = new ColumnarTable(new ColumnarTable.TableFormat(NB_OF_ATTRIBUTES, NB_OF_VALUES, CHUNK_SIZE));
+		for (int i = 0; i < TABLE_SIZE; i++) {
+			ZERO_TABLE.append(zeroRecord());
+		}
+	}
+
+	@Benchmark
+	public void findRows(Blackhole blackhole) {
+		int predicate[] = new int[NB_OF_ATTRIBUTES];
+		Arrays.fill(predicate, 0);
+		blackhole.consume(ZERO_TABLE.findRows(predicate));
+	}
+
+	@Benchmark
+	public void findRowsSIMD(Blackhole blackhole) {
+		int predicate[] = new int[NB_OF_ATTRIBUTES];
+		Arrays.fill(predicate, 0);
+		blackhole.consume(ZERO_TABLE.findRowsSIMD(predicate));
+	}
+
+	protected IRecord zeroRecord() {
+		int attributes[] = new int[NB_OF_ATTRIBUTES];
+		double values[] = new double[NB_OF_VALUES];
+		Arrays.fill(attributes, 0);
+		Arrays.fill(values, 0);
+		return new Record(attributes, values);
+	}
+}
--- src/test/java/com/activeviam/structures/table/impl/TestColumnarTable.java
+++ src/test/java/com/activeviam/structures/table/impl/TestColumnarTable.java
@@ -1,12 +1,20 @@
 package com.activeviam.structures.table.impl;
 
+import com.activeviam.chunk.IChunk;
+import com.activeviam.chunk.IChunkAllocator;
+import com.activeviam.chunk.SegmentMemoryAllocator;
 import com.activeviam.structures.bitmap.IBitmap;
 import com.activeviam.structures.bitmap.impl.BitSetBitmap;
 import com.activeviam.structures.store.impl.ColumnarTable;
 import com.activeviam.structures.store.impl.ColumnarTable.TableFormat;
 import com.activeviam.structures.store.impl.Record;
+import com.activeviam.vector.IVectorAllocator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.lang.foreign.MemorySession;
+
+import static com.activeviam.Types.INTEGER;
 
 public class TestColumnarTable {
 
@@ -28,6 +36,28 @@ public class TestColumnarTable {
 		expected.set(2);
 		expected.set(6);
 		Assertions.assertEquals(expected, table.findRows(new int[] {-1, -1, 2}));
+	}
+
+	@Test
+	public void testFindRowsSIMD() {
+		MemorySession memorySession = MemorySession.openConfined();
+		IChunkAllocator VECTOR_ALLOCATOR = new SegmentMemoryAllocator(memorySession);
+		int chunkSize = 4;
+		final ColumnarTable table = new ColumnarTable(new TableFormat(3, 2, 4), VECTOR_ALLOCATOR);
+		for (int i = 0; i < 2 * chunkSize + 1; i++) {
+			table.append(new Record(new int[] {i,  i *2, i % 4}, new double[] {i * 1D, 1D}));
+		}
+
+		table.print();
+
+		IBitmap expected = new BitSetBitmap();
+		expected.set(1);
+		Assertions.assertEquals(expected, table.findRowsSIMD(new int[] {1, 2, 1}));
+
+		expected.clear();
+		expected.set(2);
+		expected.set(6);
+		Assertions.assertEquals(expected, table.findRowsSIMD(new int[] {-1, -1, 2}));
 	}
 
 }

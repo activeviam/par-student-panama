dafda37ce2bc1fd40bb5b8310261a2623204a2ff
Paul Briand <paul.briand@student-cs.fr>
2023-03-29 14:35:18 +0200

    JmhBenchmarkColumnarTables: random table & predicat

--- src/test/java/com/activeviam/benchmark/structures/JmhBenchmarkColumnarTables.java
+++ src/test/java/com/activeviam/benchmark/structures/JmhBenchmarkColumnarTables.java
@@ -17,6 +17,7 @@ import org.openjdk.jmh.infra.Blackhole;
 
 import java.lang.foreign.MemorySession;
 import java.util.Arrays;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
@@ -28,11 +29,13 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 @Measurement(iterations = 20, time = 100, timeUnit = MILLISECONDS)
 @Fork(1)
 public class JmhBenchmarkColumnarTables {
+	protected IChunkAllocator CHUNK_ALLOCATOR;
+
+	@Param({"random", /* "zero" */})
+	protected static String TABLE_CONTENT;
+
 	@Param({"1000", "100000"})
 	protected static int TABLE_SIZE;
-	protected MemorySession memorySession;
-
-	protected IChunkAllocator CHUNK_ALLOCATOR;
 
 	@Param({"8", /* "32", "64", "128"*/})
 	protected int CHUNK_SIZE;
@@ -43,39 +46,53 @@ public class JmhBenchmarkColumnarTables {
 	@Param({"10", /* "100000", "10000000", "100000000" */})
 	protected int NB_OF_VALUES;
 
-	protected static ColumnarTable ZERO_TABLE;
+	protected static ColumnarTable TABLE;
 
 	protected int[] PREDICATE;
 	@Setup(Level.Trial)
-	public void setZeroTable() {
-		MemorySession memorySession1 = MemorySession.openConfined();
-		IChunkAllocator allocator = new SegmentMemoryAllocator(memorySession1);
-		ZERO_TABLE = new ColumnarTable(new ColumnarTable.TableFormat(NB_OF_ATTRIBUTES, NB_OF_VALUES, CHUNK_SIZE), allocator);
+	public void setChunkAllocator() {
+		MemorySession memorySession = MemorySession.openConfined();
+		CHUNK_ALLOCATOR = new SegmentMemoryAllocator(memorySession);
+	}
+	@Setup(Level.Trial)
+	public void setTable() {
+		TABLE = new ColumnarTable(new ColumnarTable.TableFormat(NB_OF_ATTRIBUTES, NB_OF_VALUES, CHUNK_SIZE), CHUNK_ALLOCATOR);
 		for (int i = 0; i < TABLE_SIZE; i++) {
-			ZERO_TABLE.append(zeroRecord());
+			TABLE.append(Record());
 		}
 		PREDICATE = new int[NB_OF_ATTRIBUTES];
-		Arrays.fill(PREDICATE, 0);
+		if (TABLE_CONTENT.equals("random")) {
+			Random rand = new Random();
+			for (int i = 0; i < NB_OF_ATTRIBUTES; i++) {
+				PREDICATE[i] = rand.nextInt(10) * rand.nextInt(2);
+			}
+		} else if (TABLE_CONTENT.equals("zero")) {
+			Arrays.fill(PREDICATE, 0);
+		}
 	}
 
 	@Benchmark
 	public void findRows(Blackhole blackhole) {
-		int predicate[] = new int[NB_OF_ATTRIBUTES];
-		Arrays.fill(predicate, 0);
-		blackhole.consume(ZERO_TABLE.findRows(predicate));
+		blackhole.consume(TABLE.findRows(PREDICATE));
 	}
 
 	@Benchmark
 	public void findRowsSIMD(Blackhole blackhole) {
-		int predicate[] = new int[NB_OF_ATTRIBUTES];
-		Arrays.fill(predicate, 0);
-		blackhole.consume(ZERO_TABLE.findRowsSIMD(predicate));
+		blackhole.consume(TABLE.findRowsSIMD(PREDICATE));
 	}
 
-	protected IRecord zeroRecord() {
+	protected IRecord Record() {
 		int attributes[] = new int[NB_OF_ATTRIBUTES];
 		double values[] = new double[NB_OF_VALUES];
-		Arrays.fill(attributes, 0);
+
+		if (TABLE_CONTENT.equals("random")) {
+			Random rand = new Random();
+			for (int i = 0; i < NB_OF_ATTRIBUTES; i++) {
+				attributes[i] = rand.nextInt(10);
+			}
+		} else if (TABLE_CONTENT.equals("zero")) {
+			Arrays.fill(attributes, 0);
+		}
 		Arrays.fill(values, 0);
 		return new Record(attributes, values);
 	}
